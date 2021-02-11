c58bbb545a735b41b838b6f5727b44d172548eee
Joan Currie <jecurrie8@gmail.com>
2021-02-11 11:43:13 -0500

    Initial commit

--- main.py
+++ main.py
@@ -1,31 +1,30 @@
-import turtle
-from turtle import Screen
-my_screen = Screen()
-t = turtle.Pen()
-t.speed(0)
-turtle.bgcolor("white")
-t.pencolor("black")
-t.width(10)
-def up():
-    t.forward(50)
-def left():
-    t.left(90)
-def right():
-    t.right(90)
-turtle.onkeypress(up,"Up")
-turtle.onkeypress(left,"Left")
-turtle.onkeypress(right,"Right")
-turtle.listen()
+from turtle import Turtle, Screen
 
-colors=["red", "orange", "yellow", "green", "blue", "purple", "gray", "brown", "aqua", "sea green"]
-your_name = turtle.textinput("Enter Your Name", "What Is Your Name?")
-sides = int(turtle.numinput("Number Of Sides", "How Many Sides Do You Want (1-10)", 5, 1, 10))
-for x in range(100):
-    t.pencolor(colors[x%sides%10])
-    t.penup()
-    t.forward(x*4)
-    t.pendown()
-    t.write(your_name, font=("Times", int( (x*2 + 4) /4), "bold"))
-    t.left(360/sides+2)
+tim = Turtle()
+my_screen = Screen()
+
+
+def move_forwards():
+    tim.forward(18)
+def move_backwards():
+    tim.backward(18)
+def turn_left():
+    new_heading = tim.heading() + 18
+    tim.setheading(new_heading)
+def turn_right():
+    new_heading = tim.heading() - 18
+    tim.setheading(new_heading)
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+my_screen.listen()
+my_screen.onkey(move_forwards, "w")
+my_screen.onkey(move_backwards, "s")
+my_screen.onkey(turn_left, "a")
+my_screen.onkey(turn_right, "d")
+my_screen.onkey(clear, "c")
 
 my_screen.exitonclick()
